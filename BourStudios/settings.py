812f0906f30db21342677c7beae0751b50cc9026
Gh05t9624 <158307320+Gh05t9624@users.noreply.github.com>
2024-07-15 13:59:36 +0000

    adresse local

--- BourStudios/settings.py
+++ BourStudios/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&x%59^v+d9ysute$+rrmhp!e_osbwe5#qq8of3^&ruhv($fosd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 AUTH_USER_MODEL = 'galsen.CustomUser'
 
