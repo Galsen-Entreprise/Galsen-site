0b8bcced1b0bbcd514e9865ab5b3d3b994c76d06
mailcoding <158386109+mailcoding@users.noreply.github.com>
2024-07-21 00:56:09 +0000

    clean

--- BourStudios/settings.py
+++ BourStudios/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 if ENVIRONMENT == 'development':
     DEBUG = True
 else:
-    DEBUG = True
+    DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
@@ -88,7 +88,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'BourStudios.wsgi.application'
+WSGI_APPLICATION = 'BourStudios.wsgi.app'
 
 # settings.py
 AUTHENTICATION_BACKENDS = [
