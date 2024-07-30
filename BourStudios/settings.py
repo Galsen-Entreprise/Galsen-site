785eb3291034e26fcafc464e392e3e11bffea9f4
mailcoding <158386109+mailcoding@users.noreply.github.com>
2024-07-30 23:49:28 +0000

    true to fallse

--- BourStudios/settings.py
+++ BourStudios/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'galsen/templates')
 
 SECRET_KEY = 'django-insecure-&x%59^v+d9ysute$+rrmhp!e_osbwe5#qq8of3^&ruhv($fosd'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '.now.sh']
 
