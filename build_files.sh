b6a5d0d30b830c01c71a8724a07f1d4b5704e9c2
mailcoding <158386109+mailcoding@users.noreply.github.com>
2024-10-20 01:24:32 +0000

    t

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
 python3.7 install collectstatic
+#tttttttttttt
 python3.7 manage.py makemigrations --noinput
 python3.7 manage.py migrate --noinput
 python3.7 manage.py collectstatic --noinput --clear
